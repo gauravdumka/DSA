66474055fe945c3af8bf6cfc46905a1b8f7c2bbc
Gaurav Dumka <gauravdumka921@gmail.com>
2026-06-09 12:30:18 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (57.47%) - LeetSync

---
+++ 136-single-number/single-number.cpp
@@ -0,0 +1,13 @@
+// brute force - iterate whole array for every number
+// better - hashing
+// optimal --> XOr method
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int XOR = 0;
+        for(int i =0 ; i< nums.size(); i++){
+            XOR = XOR^nums[i];
+        }
+        return XOR;
+    }
+};
