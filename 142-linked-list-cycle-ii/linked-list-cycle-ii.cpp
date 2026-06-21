b3dad2b7e79279aee836182fc4ea7ac6e6bfd1b9
Gaurav Dumka <gauravdumka921@gmail.com>
2026-06-21 08:15:55 +0530

    Time: 6 ms (72.25%) | Memory: 11.3 MB (85.64%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -9,17 +9,39 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        map<ListNode*, int>mpp;
-        int cnt =0 ;
-        ListNode *temp= head;
-        while(temp){
-            if(mpp.find(temp->next)!=mpp.end()){
-                return temp->next;
+        // map<ListNode*, int>mpp;
+        // int cnt =0 ;
+        // ListNode *temp= head;
+        // while(temp){
+        //     if(mpp.find(temp->next)!=mpp.end()){
+        //         return temp->next;
+        //     }
+        //     mpp[temp]=cnt;
+        //     temp= temp->next;
+        //     cnt++;
+        // }
+        // return nullptr;
+
+        // optimal solution
+        // 1-> move slow by 1 and fast by 2 from head until they collide
+        // after collision take slow to head and move both of them by one
+        // again they collide gives the answer
+        ListNode*slow=head;
+        ListNode*fast=head;
+        while(fast!=nullptr && fast->next!=nullptr){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
+                slow=head;
+                while(slow!=fast){
+                    slow=slow->next;
+                    fast=fast->next;
+                }
+                return slow;
             }
-            mpp[temp]=cnt;
-            temp= temp->next;
-            cnt++;
         }
         return nullptr;
+
+
     }
 };
