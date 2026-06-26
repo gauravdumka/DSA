78be0e959490fc7ef99bdb03e41f8fb8e15aaa3d
Gaurav Dumka <gauravdumka921@gmail.com>
2026-06-26 10:53:32 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (37.05%) - LeetSync

---
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    const int MOD=1e9+7;
+    int mypow(int x, long long n){
+        long long result=1;
+        long long half=1;
+        if(n==0)return 1;
+        if(n%2==1){
+            result=(result*x)%MOD;
+            n=n-1;
+        }
+        half=mypow(x,n/2);
+        result=result*(half*half)%MOD;
+        return result%MOD;
+    }
+    int countGoodNumbers(long long n) {
+        return (long long)mypow(5,(n+1)/2)*mypow(4,n/2)%MOD;
+
+    }
+};
