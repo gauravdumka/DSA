24ff4925e4c3a4cbe4ccb88448eae1a018853717
Gaurav Dumka <gauravdumka921@gmail.com>
2026-07-05 21:04:07 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (69.24%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.cpp
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.cpp
@@ -11,29 +11,40 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        set<int>mpp;
-        ListNode* temp=head;
+        // set<int>mpp;
+        // ListNode* temp=head;
+        // if(head==nullptr || head->next==nullptr)return head;
+        // while(temp!=nullptr){
+        //     mpp.insert(temp->val);
+        //     temp=temp->next;
+        // }
+        // // Create new linked list
+        // ListNode* newHead = nullptr;
+        // ListNode* tail = nullptr;
+
+        // for (int x : mpp) {
+        //     ListNode* newNode = new ListNode(x);
+
+        //     if (newHead == nullptr) {
+        //         newHead = newNode;
+        //         tail = newNode;
+        //     } else {
+        //         tail->next = newNode;
+        //         tail = newNode;
+        //     }
+        // }
+
+        // return newHead;
         if(head==nullptr || head->next==nullptr)return head;
-        while(temp!=nullptr){
-            mpp.insert(temp->val);
-            temp=temp->next;
-        }
-        // Create new linked list
-        ListNode* newHead = nullptr;
-        ListNode* tail = nullptr;
-
-        for (int x : mpp) {
-            ListNode* newNode = new ListNode(x);
-
-            if (newHead == nullptr) {
-                newHead = newNode;
-                tail = newNode;
-            } else {
-                tail->next = newNode;
-                tail = newNode;
+        ListNode* temp1=head; ListNode* temp2=head->next;
+        while(temp2!=nullptr){
+            if(temp1->val!=temp2->val){
+                temp1->next=temp2;
+                temp1=temp2;
             }
+            if(temp2->next==nullptr)temp1->next=nullptr;
+            temp2=temp2->next;
         }
-
-        return newHead;
+        return head;
     }
 };
