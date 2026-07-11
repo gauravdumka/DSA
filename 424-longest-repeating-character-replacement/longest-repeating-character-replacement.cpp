3ba913ed07de0ba5ba4e9512d1eebed2fba89907
Gaurav Dumka <gauravdumka921@gmail.com>
2026-07-11 18:13:31 +0530

    Time: 2 ms (72.24%) | Memory: 10.9 MB (41.53%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,16 +1,37 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
+        // int l=0; int r=0; vector<int>hash(26,0);int max_frequency_element=0;int max_len=0;
+        // while(r<s.length()){
+        //     hash[s[r]-'A']++;
+        //     max_frequency_element=max(max_frequency_element,hash[s[r]-'A']);
+        //     while((((r-l)+1)-max_frequency_element)>k){
+        //         hash[s[l]-'A']--;
+        //         l++; max_frequency_element=0;
+        //             ////  REMOVE BELOW FOE LOOP TO GET SECOND OPTIMAL SOLUTION
+        //         //// for(int i=0; i<26;i++){
+        //         ////     max_frequency_element=max(hash[i],max_frequency_element);
+        //         //// }
+        //     }
+        //     if((((r-l)+1)-max_frequency_element)<=k){
+        //         int len=(r-l)+1;
+        //         max_len=max(len,max_len);
+        //     }
+        //     r++;
+        // }
+        // return max_len;
+        ////// TC=O((n+n)*26) , SC=O(26)
+        ////// so remove the above for loop , now TC=O(n+n)
+
+
+        //// now most optimized sol of TC=O(n)
         int l=0; int r=0; vector<int>hash(26,0);int max_frequency_element=0;int max_len=0;
         while(r<s.length()){
             hash[s[r]-'A']++;
             max_frequency_element=max(max_frequency_element,hash[s[r]-'A']);
-            while((((r-l)+1)-max_frequency_element)>k){
+            if((((r-l)+1)-max_frequency_element)>k){
                 hash[s[l]-'A']--;
-                l++; max_frequency_element=0;
-                for(int i=0; i<26;i++){
-                    max_frequency_element=max(hash[i],max_frequency_element);
-                }
+                l++; 
             }
             if((((r-l)+1)-max_frequency_element)<=k){
                 int len=(r-l)+1;
@@ -18,6 +39,6 @@ public:
             }
             r++;
         }
-        return max_len;
+        return max_len;  //// TC=O(n)
     }
 };
