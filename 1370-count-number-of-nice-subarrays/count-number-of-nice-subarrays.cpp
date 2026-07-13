c6b958ae67dd4a72b2c2ee8eb988a19188c91cbd
Gaurav Dumka <gauravdumka921@gmail.com>
2026-07-13 23:11:02 +0530

    Time: 20 ms (22.17%) | Memory: 77.7 MB (23.57%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int count_goal(vector<int>&nums, int goal){
+    int count_goal(vector<int>nums, int goal){
         int l=0; int r=0; int sum=0; int cnt=0;
         while(r<nums.size()){
             if(goal<0) return 0;
