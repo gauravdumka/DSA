30a06902779e40a8fc2db271dbd6e8a5f0101c18
Gaurav Dumka <gauravdumka921@gmail.com>
2026-07-25 07:20:22 +0530

    Time: 23 ms (87.00%) | Memory: 91.9 MB (91.35%) - LeetSync

--- 937-online-stock-span/online-stock-span.cpp
+++ 937-online-stock-span/online-stock-span.cpp
@@ -1,23 +1,40 @@
-class StockSpanner {
-public:
-    vector<int>prices; stack<int>st;
-    StockSpanner() {
-        vector<int>prices; stack<int>st;
-    }
+// class StockSpanner {
+// public:
+//     vector<int>prices; stack<int>st;
+//     StockSpanner() {
+//         vector<int>prices; stack<int>st;
+//     }
     
-    int next(int price) {
-        prices.push_back(price); int span;
-        while(!st.empty() && prices[st.top()]<=price){
+//     int next(int price) {
+//         prices.push_back(price); int span;
+//         while(!st.empty() && prices[st.top()]<=price){
+//             st.pop();
+//         }
+//         if (st.empty()){
+//              span = prices.size();
+//         }else{
+//              span = (prices.size() - 1) - st.top();
+//         }
+//         st.push(prices.size()-1);
+//         return span;
+//     }     
+// };
+class StockSpanner{
+public:
+    stack<pair<int,int>>st; int idx=-1;
+    StockSpanner(){
+        idx=-1;
+    }
+    int next(int price){
+        int ans=0;
+        idx++;
+        while(!st.empty() && (st.top().first<=price)){
             st.pop();
         }
-        if (st.empty()){
-             span = prices.size();
-        }else{
-             span = (prices.size() - 1) - st.top();
-        }
-        st.push(prices.size()-1);
-        return span;
-    }     
+        ans= idx-(st.empty()?-1:st.top().second);
+        st.push({price,idx});
+        return ans;
+    }
 };
 
 /**
