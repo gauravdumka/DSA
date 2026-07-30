9a7c093482e9e964aa4e1e34a0c3893fdb185822
Gaurav Dumka <gauravdumka921@gmail.com>
2026-07-30 22:12:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (54.67%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        int result=0;
-        unordered_map<int,int>mp;
-        int assignkey=2;
+        // int result=0;
+        // unordered_map<int,int>mp;
+        // int assignkey=2;
+        // for(char &ch:word){
+        //     if(assignkey>9){
+        //         assignkey=2;
+        //     }
+        //     mp[assignkey]++;
+        //     result+=mp[assignkey];
+        //     assignkey++;
+        // }
+        // return result;
+
+        vector<int>hash(26); int result=0;
         for(char &ch:word){
-            if(assignkey>9){
-                assignkey=2;
-            }
-            mp[assignkey]++;
-            result+=mp[assignkey];
-            assignkey++;
+            hash[ch-'a']++;
+        }
+        sort(hash.begin(),hash.end(),greater<int>());
+        for(int i=0;i<26;i++){
+            result+=((i/8)+1)*hash[i];
         }
         return result;
     }
