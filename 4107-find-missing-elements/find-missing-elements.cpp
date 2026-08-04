f397ca4e341991caf65d79ed12e31cfd3d29c7e7
Gaurav Dumka <gauravdumka921@gmail.com>
2026-08-04 19:36:37 +0530

    Time: 2 ms (55.23%) | Memory: 32.3 MB (75.54%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.cpp
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
-        int mini=INT_MAX; int maxi=INT_MIN;
-        for(int i=0;i<nums.size();i++){
-            mini=min(mini,nums[i]);
-            maxi=max(maxi,nums[i]);
+        // int mini=INT_MAX; int maxi=INT_MIN;
+        // for(int i=0;i<nums.size();i++){
+        //     mini=min(mini,nums[i]);
+        //     maxi=max(maxi,nums[i]);
 
-        }
-        vector<int>arr(maxi+1,0);
-        for(int i=0;i<nums.size();i++){
-            arr[nums[i]]++;
-        }
-        vector<int>ans;
-        for(int i=mini;i<=maxi;i++){
-            if(arr[i]==0){
-                ans.push_back(i);
+        // }
+        // vector<int>arr(maxi+1,0);
+        // for(int i=0;i<nums.size();i++){
+        //     arr[nums[i]]++;
+        // }
+        // vector<int>ans;
+        // for(int i=mini;i<=maxi;i++){
+        //     if(arr[i]==0){
+        //         ans.push_back(i);
+        //     }
+        // }
+        // return ans;
+        sort(nums.begin(),nums.end());
+        vector<int>result;
+        for(int i=1;i<nums.size();i++){
+            for(int j=nums[i-1]+1;j<nums[i];j++){
+                result.push_back(j);
             }
         }
-        return ans;
+        return result;
 
     }
 };
