f91ed9287f67679d548049311bac8a1304d22fd1
Gaurav Dumka <gauravdumka921@gmail.com>
2026-08-04 06:17:31 +0530

    Time: 3 ms (67.91%) | Memory: 10.7 MB (93.76%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -32,10 +32,10 @@ public:
         max_freq_el=max(max_freq_el,hash[s[j]-'A']);
         while((j-i+1)-max_freq_el>k){
             hash[s[i]-'A']--;
-            max_freq_el=0;
-            for(int l=0;l<26;l++){
-                max_freq_el=max(max_freq_el,hash[l]);
-            }
+            // max_freq_el=0;
+            // for(int l=0;l<26;l++){
+            //     max_freq_el=max(max_freq_el,hash[l]);
+            // }
             i++;
         }
         if((j-i+1)-max_freq_el<=k){
