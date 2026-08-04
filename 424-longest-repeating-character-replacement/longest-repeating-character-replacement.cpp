6362c03241ead54d47b8efa9b4d0c6f0d38eccb6
Gaurav Dumka <gauravdumka921@gmail.com>
2026-08-04 06:17:03 +0530

    Time: 11 ms (29.36%) | Memory: 10.7 MB (93.76%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -25,20 +25,24 @@ public:
 
 
         //// now most optimized sol of TC=O(n)
-        int l=0; int r=0; vector<int>hash(26,0);int max_frequency_element=0;int max_len=0;
-        while(r<s.length()){
-            hash[s[r]-'A']++;
-            max_frequency_element=max(max_frequency_element,hash[s[r]-'A']);
-            if((((r-l)+1)-max_frequency_element)>k){
-                hash[s[l]-'A']--;
-                l++; 
+       
+       int i=0; int j=0;int maxlen=0; int max_freq_el=0;vector<int>hash(26,0);
+       while(j<s.length()){
+        hash[s[j]-'A']++;
+        max_freq_el=max(max_freq_el,hash[s[j]-'A']);
+        while((j-i+1)-max_freq_el>k){
+            hash[s[i]-'A']--;
+            max_freq_el=0;
+            for(int l=0;l<26;l++){
+                max_freq_el=max(max_freq_el,hash[l]);
             }
-            if((((r-l)+1)-max_frequency_element)<=k){
-                int len=(r-l)+1;
-                max_len=max(len,max_len);
-            }
-            r++;
+            i++;
         }
-        return max_len;  //// TC=O(n)
+        if((j-i+1)-max_freq_el<=k){
+            maxlen=max(maxlen,j-i+1);
+        }
+        j++;
+       }
+       return maxlen;
     }
 };
