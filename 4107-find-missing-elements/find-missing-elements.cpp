387f9c44f3d7cce250d731531aca62e601ddab69
Gaurav Dumka <gauravdumka921@gmail.com>
2026-08-04 19:45:48 +0530

    Time: 8 ms (12.18%) | Memory: 33.9 MB (5.29%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.cpp
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -1,31 +1,49 @@
 class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
-        // int mini=INT_MAX; int maxi=INT_MIN;
-        // for(int i=0;i<nums.size();i++){
-        //     mini=min(mini,nums[i]);
-        //     maxi=max(maxi,nums[i]);
+//         int mini=*min_element(nums.begin(),nums.end()); int maxi=*max_element(nums.begin(),nums.end());
+//         // for(int i=0;i<nums.size();i++){
+//         //     mini=min(mini,nums[i]);
+//         //     maxi=max(maxi,nums[i]);
 
-        // }
-        // vector<int>arr(maxi+1,0);
-        // for(int i=0;i<nums.size();i++){
-        //     arr[nums[i]]++;
-        // }
-        // vector<int>ans;
-        // for(int i=mini;i<=maxi;i++){
-        //     if(arr[i]==0){
-        //         ans.push_back(i);
-        //     }
-        // }
-        // return ans;
-        sort(nums.begin(),nums.end());
-        vector<int>result;
-        for(int i=1;i<nums.size();i++){
-            for(int j=nums[i-1]+1;j<nums[i];j++){
-                result.push_back(j);
+//         // }
+//         vector<int>arr(maxi+1,0);
+//         for(int i=0;i<nums.size();i++){
+//             arr[nums[i]]++;
+//         }
+//         vector<int>ans;
+//         for(int i=mini;i<=maxi;i++){
+//             if(arr[i]==0){
+//                 ans.push_back(i);
+//             }
+//         }
+//         return ans;
+
+//                   ////// SECOND METHOD
+//         // sort(nums.begin(),nums.end());
+//         // vector<int>result;
+//         // for(int i=1;i<nums.size();i++){
+//         //     for(int j=nums[i-1]+1;j<nums[i];j++){
+//         //         result.push_back(j);
+//         //     }
+//         // }
+//         // return result;
+
+
+                   ///////  third method
+             set<int> s(nums.begin(), nums.end());
+
+        int mini = *min_element(nums.begin(), nums.end());
+        int maxi = *max_element(nums.begin(), nums.end());
+
+        vector<int> ans;
+
+        for(int i = mini; i <= maxi; i++) {
+            if(s.count(i) == 0) {
+                ans.push_back(i);
             }
         }
-        return result;
 
+        return ans;
     }
 };
