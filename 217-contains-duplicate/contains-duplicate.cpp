c1022db03665dc07f661ebc48533cd1ae74f7198
Gaurav Dumka <gauravdumka921@gmail.com>
2026-08-07 06:34:35 +0530

    Time: 71 ms (45.46%) | Memory: 111.3 MB (31.92%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int>st;
+        for(int x : nums) {
+            if(!st.insert(x).second)
+                return true;
+        }
+
+        return false;
+    }
+};
