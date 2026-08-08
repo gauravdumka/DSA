521e07e703c047197cce6a1b0b7fcdfc972f6b65
Gaurav Dumka <gauravdumka921@gmail.com>
2026-08-08 08:02:40 +0530

    Time: 155 ms (6.99%) | Memory: 12 MB (5.80%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,53 +1,88 @@
 class Solution {
 public:
-    string minWindow(string s, string t) {
-        int low = 0;
-        int high = 0;
-
-        vector<int> hash(256, 0);
-        vector<int> hash1(256, 0);
-
-        int n = s.length();
-        int idx = -1;
-        int len = INT_MAX;
-
-        for(int i = 0; i < t.length(); i++) {
-            hash1[t[i]]++;
-        }
-
-        int count = 0;
-
-        while(high < n) {
-
-            hash[s[high]]++;
-
-            if(hash1[s[high]] > 0 &&
-               hash[s[high]] <= hash1[s[high]]) {
-                count++;
+    bool check(vector<int>&hash1, vector<int>&hash2){
+        bool flag=true;
+        for(int i=0; i<256;i++){
+            if(hash1[i]>0 &&(hash2[i]<hash1[i])){
+                flag=false;
             }
+        }
+            return flag;
+    }
+    string minWindow(string s, string t) {
+        // int low = 0;
+        // int high = 0;
 
-            while(count == t.length()) {
+        // vector<int> hash(256, 0);
+        // vector<int> hash1(256, 0);
 
-                if(high - low + 1 < len) {
-                    len = high - low + 1;
-                    idx = low;
-                }
+        // int n = s.length();
+        // int idx = -1;
+        // int len = INT_MAX;
 
-                if(hash1[s[low]] > 0 &&
-                   hash[s[low]] <= hash1[s[low]]) {
-                    count--;
-                }
+        // for(int i = 0; i < t.length(); i++) {
+        //     hash1[t[i]]++;
+        // }
 
-                hash[s[low]]--;
+        // int count = 0;
+
+        // while(high < n) {
+
+        //     hash[s[high]]++;
+
+        //     if(hash1[s[high]] > 0 &&
+        //        hash[s[high]] <= hash1[s[high]]) {
+        //         count++;
+        //     }
+
+        //     while(count == t.length()) {
+
+        //         if(high - low + 1 < len) {
+        //             len = high - low + 1;
+        //             idx = low;
+        //         }
+
+        //         if(hash1[s[low]] > 0 &&
+        //            hash[s[low]] <= hash1[s[low]]) {
+        //             count--;
+        //         }
+
+        //         hash[s[low]]--;
+        //         low++;
+        //     }
+
+        //     high++;
+        // }
+
+        // if(idx == -1)
+        //     return "";
+
+        // return s.substr(idx, len);
+
+
+
+
+
+
+        
+
+        int low=0; int high=0; vector<int>hash1(256,0);
+        for(int i=0;i<t.length();i++){
+            hash1[t[i]]++;
+        } 
+        int idx=-1; vector<int>hash2(256,0); int len=INT_MAX;
+        while(high<s.length()){
+            hash2[s[high]]++;
+            while(check(hash1,hash2)){
+                if(high-low+1 < len) {
+                len = high-low+1;
+                idx = low;
+            }
+                hash2[s[low]]--;
                 low++;
             }
-
             high++;
         }
-
-        if(idx == -1)
-            return "";
-
-        return s.substr(idx, len);
+        return(idx==-1?"":s.substr(idx,len));
     }
 };
