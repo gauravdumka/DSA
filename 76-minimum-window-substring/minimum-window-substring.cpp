49c99e53a58af84a5b50f55b5d709c70e2dab113
Gaurav Dumka <gauravdumka921@gmail.com>
2026-08-08 07:44:57 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (15.89%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,32 +1,53 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int m=s.length();
-        int n=t.length();
-        int l=0;int r=0;int min_len=INT_MAX; int cnt=0;int Sindex=-1;
-        int hash[256]={0};
-        for(int i=0; i<t.length();i++){
-            hash[t[i]]++;
+        int low = 0;
+        int high = 0;
+
+        vector<int> hash(256, 0);
+        vector<int> hash1(256, 0);
+
+        int n = s.length();
+        int idx = -1;
+        int len = INT_MAX;
+
+        for(int i = 0; i < t.length(); i++) {
+            hash1[t[i]]++;
         }
-        while(r<s.length()){
-            if(hash[s[r]]>0){
-                cnt++;
+
+        int count = 0;
+
+        while(high < n) {
+
+            hash[s[high]]++;
+
+            if(hash1[s[high]] > 0 &&
+               hash[s[high]] <= hash1[s[high]]) {
+                count++;
             }
-            hash[s[r]]--;
-            while(cnt==n){
-                ////// shrink the window for min substring
-                if(((r-l)+1)<min_len){
-                    min_len=(r-l)+1;
-                    Sindex=l;
+
+            while(count == t.length()) {
+
+                if(high - low + 1 < len) {
+                    len = high - low + 1;
+                    idx = low;
                 }
-                hash[s[l]]++;
-                if(hash[s[l]]>0){
-                    cnt--;
+
+                if(hash1[s[low]] > 0 &&
+                   hash[s[low]] <= hash1[s[low]]) {
+                    count--;
                 }
-                l++;
+
+                hash[s[low]]--;
+                low++;
             }
-            r++;
+
+            high++;
         }
-        return(Sindex==-1?"":s.substr(Sindex,min_len));
+
+        if(idx == -1)
+            return "";
+
+        return s.substr(idx, len);
     }
 };
