33b85ba68d7adcaf95b4668c40d4273ff01e687b
Gaurav Dumka <gauravdumka921@gmail.com>
2026-08-09 11:08:17 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (68.43%) - LeetSync

--- 202-happy-number/happy-number.cpp
+++ 202-happy-number/happy-number.cpp
@@ -1,20 +1,51 @@
+// class Solution {
+// public:
+//     bool isHappy(int n) {
+//         unordered_set<int>st;
+//         while(n!=1){
+//             if(st.find(n) != st.end()) {
+//                 return false;   // n appeared before → cycle
+//             }
+//             st.insert(n);
+//             int sum=0;
+//             while(n){
+//                 int rem=n%10;
+//                 sum+=rem*rem;
+//                 n=n/10;
+//             }
+//             n=sum;
+//         }
+//         return true;
+//     }
+// };
+
+
 class Solution {
 public:
-    bool isHappy(int n) {
-        unordered_set<int>st;
-        while(n!=1){
-            if(st.find(n) != st.end()) {
-                return false;   // n appeared before → cycle
-            }
-            st.insert(n);
-            int sum=0;
-            while(n){
-                int rem=n%10;
-                sum+=rem*rem;
-                n=n/10;
-            }
-            n=sum;
+    int nextNum(int n) {
+        int sum = 0;
+
+        while(n) {
+            int rem = n % 10;
+            sum += rem * rem;
+            n /= 10;
         }
-        return true;
+
+        return sum;
+    }
+
+    bool isHappy(int n) {
+        int slow = n;
+        int fast = n;
+
+        slow = nextNum(slow);
+        fast = nextNum(nextNum(fast));
+
+        while(slow != fast) {
+            slow = nextNum(slow);
+            fast = nextNum(nextNum(fast));
+        }
+
+        return slow == 1;
     }
 };
