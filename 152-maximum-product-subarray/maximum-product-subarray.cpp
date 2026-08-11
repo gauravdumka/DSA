6d0f8b46adb58e1643680c0094cd5482e4d4cb0b
Gaurav Dumka <gauravdumka921@gmail.com>
2026-08-11 21:32:11 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (87.72%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -3,9 +3,9 @@ public:
     
     int maxProduct(vector<int>& nums) {
         int minending =nums[0]; int maxending=nums[0]; int result=nums[0];
-        if(nums.size()==1){
-            return nums[0];
-        }
+        // if(nums.size()==1){
+        //     return nums[0];
+        // }
         for(int i=1;i<nums.size();i++){
             int v1=nums[i];
             int v2=nums[i]*minending;
