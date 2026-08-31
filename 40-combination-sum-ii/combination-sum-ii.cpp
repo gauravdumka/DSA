5f31b040f8faf29fefc9a6c5e3d7ad680d4874c2
Gaurav Dumka <gauravdumka921@gmail.com>
2026-08-31 20:09:45 +0530

    Time: 3 ms (37.56%) | Memory: 14.2 MB (40.16%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void find_subsequence(int idx,int target,vector<int>&arr ,vector<int>&temp, vector<vector<int>>&ans){
+        if(target==0){
+            ans.push_back(temp);
+            return;
+        }
+        if(idx==arr.size()){
+            return;
+        }
+        for(int i=idx;i<arr.size();i++){
+            if((i>idx) && (arr[i]==arr[i-1]))continue;
+            if(arr[i]>target)break;
+
+            temp.push_back(arr[i]);
+            find_subsequence(i+1,target-arr[i],arr,temp,ans);
+            temp.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>>ans;
+        vector<int>temp;
+        sort(candidates.begin(),candidates.end());
+        find_subsequence(0,target,candidates,temp,ans);
+        return ans;
+    }
+};
