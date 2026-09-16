75d5fb4b8461489650090a45c2cbd2fd1f0365fb
Gaurav Dumka <gauravdumka921@gmail.com>
2026-09-16 05:34:13 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (13.12%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -13,7 +13,7 @@ class Solution {
 public:
     bool identical(TreeNode* p, TreeNode* q){
         if(p==nullptr||q==nullptr)return p==q;
-        if(p->val!=q->val)return false;
+        // if(p->val!=q->val)return false;
 
         return(identical(p->left,q->left) &&  identical(p->right,q->right) && p->val==q->val);
         
