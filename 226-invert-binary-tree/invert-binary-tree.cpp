8cf200b762899299cdf250d89592e0ba8a0dbeae
Gaurav Dumka <gauravdumka921@gmail.com>
2026-09-19 18:25:03 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (61.45%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.cpp
+++ 226-invert-binary-tree/invert-binary-tree.cpp
@@ -9,22 +9,34 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+// class Solution {
+// public:
+//     TreeNode* invertTree(TreeNode* root) {
+//         queue<TreeNode*>q;
+//         if(root==nullptr)return root;
+//         q.push(root);
+//         while(!q.empty()){
+//             TreeNode* node=q.front(); q.pop();
+//             TreeNode*temp=node->left;
+//             node->left=node->right;
+//             node->right=temp;
+//             if(node->left != nullptr)
+//                 q.push(node->left);
+//             if(node->right != nullptr)
+//                 q.push(node->right);     
+//         }
+//         return root;
+//     }
+// };
+class Solution{
 public:
-    TreeNode* invertTree(TreeNode* root) {
-        queue<TreeNode*>q;
-        if(root==nullptr)return root;
-        q.push(root);
-        while(!q.empty()){
-            TreeNode* node=q.front(); q.pop();
-            TreeNode*temp=node->left;
-            node->left=node->right;
-            node->right=temp;
-            if(node->left != nullptr)
-                q.push(node->left);
-            if(node->right != nullptr)
-                q.push(node->right);     
-        }
+    TreeNode* invertTree(TreeNode* root){
+        if(root==NULL)return nullptr;
+        TreeNode* temp=root->left;
+        root->left=root->right;
+        root->right=temp;
+        invertTree(root->left);
+        invertTree(root->right);
         return root;
     }
 };
